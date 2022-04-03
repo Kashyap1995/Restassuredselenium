e2116b4425177d985e9caa73e6e022e08456e8c6
Kashyap1995 <kkdave1995@gmaill.com>
2022-04-03 17:52:25 +0530

    first commit

---
+++ Mavenrestassured/src/test/java/org/firstrestassured/Mavenrestassured/Post_method.java
@@ -0,0 +1,39 @@
+package org.firstrestassured.Mavenrestassured;
+
+import org.testng.annotations.Test;
+import org.json.simple.JSONObject;
+import io.restassured.http.ContentType;
+import static io.restassured.RestAssured.*;
+
+public class Post_method {
+
+	@Test
+	public void post_data() {
+		
+		//Map<String, Object> map = new HashMap<String, Object>();
+		
+//		map.put("name", "kashyap");
+//		map.put("job", "QA");
+//		System.out.println(map);
+		
+		JSONObject request = new JSONObject();
+		request.put("name", "kashyap");
+		request.put("job", "QA");
+
+		System.out.println(request.toJSONString());
+
+		baseURI="https://reqres.in/api";
+
+		
+		given().
+		header("Content-Type","applicationjason").
+		contentType(ContentType.JSON).
+		accept(ContentType.JSON).
+		body(request.toJSONString()).
+		when().
+		post("users/2").
+		then().
+		statusCode(201).
+		log().all();
+	}
+}
